6fdea925d86e913c61148efa9e6d1628f4951a91
kulinichx <kulinich@126.com>
2026-09-16 15:09:38 +0800

    runtime: remove legacy iOS 15 dyldhook path

--- BaseBin/libjailbreak/src/basebin_gen.m
+++ BaseBin/libjailbreak/src/basebin_gen.m
@@ -61,21 +61,12 @@ NSString *dyldhook_dylib_for_platform(void)
 	cpu_subtype_t cpusubtype = 0;
 	size_t len = sizeof(cpusubtype);
 	if (sysctlbyname("hw.cpusubtype", &cpusubtype, &len, NULL, 0) == -1) { return nil; }
+
 	if ((cpusubtype & ~CPU_SUBTYPE_MASK) == CPU_SUBTYPE_ARM64E) {
-		if (@available(iOS 16.0, *)) {
-			return @"dyldhook_merge.arm64e.dylib"; 
-		}
-		else {
-			return @"dyldhook_merge.arm64e.iOS15.dylib"; 
-		}
+		return @"dyldhook_merge.arm64e.dylib";
 	}
 	else {
-		if (@available(iOS 16.0, *)) {
-			return @"dyldhook_merge.arm64.dylib"; 
-		}
-		else {
-			return @"dyldhook_merge.arm64.iOS15.dylib"; 
-		}
+		return @"dyldhook_merge.arm64.dylib";
 	}
 }
 
